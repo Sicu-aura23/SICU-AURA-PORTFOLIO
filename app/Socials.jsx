f28d366af09e37da19b69c3ad87fa7198556f9c3
Sicu-aura23 <155062207+Sicu-aura23@users.noreply.github.com>
2024-01-24 00:37:43 +0900

    added links

--- app/Socials.jsx
+++ app/Socials.jsx
@@ -11,8 +11,8 @@ const Socials = ({ top }) => {
         <div className={`flex flex-col items-center justify-around gap-6 top-${top} top-44 left-0 fixed z-10 `}>
             <Image className=' hover:cursor-pointer' onClick={()=>window.open('https://www.linkedin.com/company/sicu-aura/')}  src={linkedin} />
             <Image className=' hover:cursor-pointer' onClick={()=>window.open('https://www.facebook.com/profile.php?id=100092369332099&mibextid=LQQJ4d')} src={facebook} /> 
-            <Image  src={whatsapp} className="text-2xl " />
-            <Image  src={instagram} className=' hover:cursor-pointer' onClick={()=>window.open('https://www.instagram.com/dream_is_live_/')}  />
+            <Image  src={whatsapp} onClick={()=>window.open('https://whatsapp.com/channel/0029Va90UM46RGJGdjSrNH3E')} className="text-2xl hover:cursor-pointer " />
+            <Image  src={instagram} className=' hover:cursor-pointer' onClick={()=>window.open('https://instagram.com/sicu.aura')}  />
         </div>
     )
 }
