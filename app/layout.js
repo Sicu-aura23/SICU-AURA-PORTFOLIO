350f93f603d85fa94cbfd9d8a41ff22ce9692445
Sicu-aura23 <155062207+Sicu-aura23@users.noreply.github.com>
2024-01-24 01:14:06 +0900

    fixed meta tags

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@ import GoogleAnalytics from './GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] })
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -14,11 +13,6 @@ export default function RootLayout({ children }) {
        <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Sicu-Aura : Your one stop safety solutions from India</title>
-
-
-   
-  <meta name="description" content="Sicu-aura is a pre-revenue company which is working so hard to bring innovative technical solution as a Next Generation SoS. Our aim is to reduce rape cases, accidental death cases, river death cases, harrassment and bullying cases by 90%. We are also dedicated to bring out and serve justice to unregister cases due to faceloss. There are many more features. So stay tuned to our social media to stay updated"/>
   <meta name="keywords" content="safety, security, protection, prevention, guidelines, Safety, Sicu-aura, Sicuaura, Sicura, Sicure-Rah, Sicurerah, Rishav Ghosh, Feel Safe Everywhere,  Safer, Modern Technologies, #Safe-T-Fast, sicu-aura.com, Sicuaura.com, siquara, Safety tips, Safety precautions, Safety measures, Safety guidelines, Safety equipment, Safety training, Safety regulations, Safety procedures, Safety in the workplace, Safety awareness, Safety practices, Safety checklist, Safety rules, Safety culture, Emergency SOS, SOS app, SOS button, SOS emergency, SOS signal, safety related unique company, Top Exclusive startup, Next Billionaires"/>
   
  
